9f635ba24164dea04131d42f5b1942172c57ae8d
Meghdeepsinh  Chudasama <110728369+meghdeep18@users.noreply.github.com>
2024-08-16 10:50:55 +0530

    🍃updated

--- script.js
+++ script.js
@@ -328,7 +328,7 @@ function anim(){
     })
 
     tl2.to(".main", {
-        backgroundColor: "#000080",
+        backgroundColor: "maroon",
         color: "#fff",
     })
 
@@ -475,4 +475,28 @@ function magneticEffect( element, distance){
 
 document.addEventListener('mousemove', function(e){
     magneticEffect(document.querySelector('#nav #circle'), 100);
+})
+
+var initialPath =`M 10 100 Q 500 100 1200 100` 
+
+var finalPath = `M 10 100 Q 500 100 1200 100`
+
+var string=document.querySelector("#string")
+
+string.addEventListener("mousemove",function(dets){
+    path =`M 10 100 Q ${dets.x} ${dets.y} 1200 100`
+    gsap.to("svg path",{
+        attr: { d : path },
+        duration:0.3,
+        ease:"power3.out"
+    })
+})
+
+string.addEventListener("mouseleave",function(){
+    // path =`M 10 100 Q 500 ${dets.y} 999 100`
+    gsap.to("svg path",{
+        attr: {d:finalPath},
+        duration:1,
+        ease:"bounce.out"
+    })
 })
